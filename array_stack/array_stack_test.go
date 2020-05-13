7db44fc8725f2809967668343476fa970c112f47
takenakasuji <t.nksj.e.89@gmail.com>
2020-05-13 10:27:58 +0900

    add some tests

--- array_stack/array_stack_test.go
+++ array_stack/array_stack_test.go
@@ -1,6 +1,7 @@
 package array_stack
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -18,5 +19,28 @@ func TestSet(t *testing.T) {
 	r := as.Set(0, 1)
 	if r != 1 {
 		t.Errorf("ArrayStack.Set() = %v", r)
+	}
+}
+
+func TestGet(t *testing.T) {
+	tests := []int{100, 1, 20}
+
+	as := New()
+	as.buf = tests
+	as.Set(2, 200)
+	r := as.Get(2)
+	if r != 200 {
+		t.Errorf("ArrayStack.Get() = %v", r)
+	}
+}
+
+func TestAdd(t *testing.T) {
+	tests := []int{100, 1, 20}
+
+	as := New()
+	as.buf = append([]int{}, tests...)
+	as.Add(2, 200)
+	if reflect.DeepEqual(tests, as.buf) {
+		t.Errorf("ArrayStack.Add() = %v, %v", tests, as.buf)
 	}
 }
