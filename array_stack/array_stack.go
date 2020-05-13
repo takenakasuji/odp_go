57ffb9fa12d450e3ebc2bfbdde5e28375c185fa3
takenakasuji <t.nksj.e.89@gmail.com>
2020-05-13 18:33:31 +0900

    fix add and remove function

--- array_stack/array_stack.go
+++ array_stack/array_stack.go
@@ -2,7 +2,7 @@ package array_stack
 
 type ArrayStack struct {
 	n, cap int
-	buf []int
+	buf    []int
 }
 
 func New() ArrayStack {
@@ -26,38 +26,31 @@ func (as *ArrayStack) Add(i, v int) {
 	if as.isFull() {
 		as.resize()
 	}
-	for s := as.n; s > i; s-- {
-		as.buf[s] = as.buf[s-1]
-	}
+	as.buf = append(as.buf[:i+1], as.buf[i:]...)
 	as.buf[i] = v
-	as.n++
 }
 
 func (as *ArrayStack) Remove(i int) int {
 	r := as.buf[i]
-	for s := i; s < as.n - 1; s++ {
-		as.buf[s] = as.buf[s + 1]
-	}
-	as.n--
+	as.buf = append(as.buf[:i], as.buf[i+1:]...)
 	if as.isSeparate() {
-
 		as.resize()
 	}
 	return r
 }
 
 func (as ArrayStack) isSeparate() bool {
-	return len(as.buf) >= as.n * 3
+	return cap(as.buf) >= len(as.buf)*3
 }
 
 func (as ArrayStack) isFull() bool {
-	return as.n == as.cap
+	return len(as.buf) == cap(as.buf)
 }
 
 func (as ArrayStack) resize() {
-	as.cap = as.maximum(as.n * 2, 1)
-	newBuf := make([]int, as.cap)
-	for i := 0; i < as.n; i++ {
+	c := as.maximum(len(as.buf)*2, 1)
+	newBuf := make([]int, len(as.buf), c)
+	for i := 0; i < len(as.buf); i++ {
 		newBuf[i] = as.buf[i]
 	}
 	as.buf = newBuf
