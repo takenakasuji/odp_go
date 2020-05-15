90088da78c1d7a25548796c81057c573720b33f9
takenakasuji <t.nksj.e.89@gmail.com>
2020-05-16 00:11:25 +0900

    create array_queue

--- array_queue/array_queue.go
+++ array_queue/array_queue.go
@@ -1,9 +1,55 @@
 package array_queue
 
 type ArrayQueue struct {
-	buf []int
+	n, cap, j int
+	buf       []int
 }
 
 func New() ArrayQueue {
 	return ArrayQueue{}
+}
+
+func (as *ArrayQueue) Add(v int) {
+	if as.isFull() {
+		as.resize()
+	}
+	as.buf[(as.j+as.n)%as.cap] = v
+	as.n++
+}
+
+func (as *ArrayQueue) Remove() int {
+	ret := as.buf[as.j]
+	as.j = (as.j + 1) % as.cap
+	as.n--
+	if as.isSparse() {
+		as.resize()
+	}
+	return ret
+}
+
+func (as ArrayQueue) isFull() bool {
+	return as.n == as.cap
+}
+
+func (as ArrayQueue) isSparse() bool {
+	return len(as.buf) >= 3*as.n
+}
+
+func (as *ArrayQueue) resize() {
+	capNew := as.maximum(2*as.n, 1)
+	bufNew := make([]int, capNew)
+	for i := 0; i < as.n; i++ {
+		bufNew[i] = as.buf[(i+as.j)%as.cap]
+	}
+	as.buf = bufNew
+	as.cap = capNew
+	as.j = 0
+}
+
+func (as ArrayQueue) maximum(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
 }
--- array_queue/array_queue_test.go
+++ array_queue/array_queue_test.go
@@ -1,1 +1,61 @@
 package array_queue
+
+import (
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	if ret := New().n; ret != 0 {
+		t.Errorf("ArrayQueue.New().n = %v", ret)
+	}
+}
+
+func TestAdd(t *testing.T) {
+	tests := []int{1, 2, 1, -1, -2, -100, 0, 0}
+	aq := New()
+	for i, v := range tests {
+		aq.Add(v)
+		if aq.n != i+1 {
+			t.Errorf("aq.n = %v at %v th Add", aq.n, i+1)
+		}
+	}
+}
+
+func TestAddMany(t *testing.T) {
+	tests := make([]int, 12345)
+	aq := New()
+
+	for i, v := range tests {
+		aq.Add(v)
+		if aq.n != i+1 {
+			t.Errorf("aq.n = %v at %v th Add", aq.n, i+1)
+		}
+	}
+}
+
+func TestAddAndRemove(t *testing.T) {
+	tests := []int{1, 2, 1, -1, -2, -100, 0, 0}
+	aq := New()
+
+	for _, v := range tests {
+		aq.Add(v)
+		if ret := aq.Remove(); ret != v {
+			t.Errorf("Add %v, then %v was removed", v, ret)
+		}
+	}
+}
+
+func TestAddAndRemove2(t *testing.T) {
+	n := 123
+	aq := New()
+
+	for i := 0; i < n; i++ {
+		aq.Add(i)
+	}
+
+	for i := 0; i < n; i++ {
+		if ret, want := aq.Remove(), i; ret != want {
+			t.Errorf("expect %d but returned %d", ret, want)
+		}
+	}
+}
