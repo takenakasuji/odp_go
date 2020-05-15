d900d34dbe6998f4f6fd054c26f4a2b78610c7ff
takenakasuji <t.nksj.e.89@gmail.com>
2020-05-16 00:58:17 +0900

    change Add test and create resize test

--- array_queue/array_queue_test.go
+++ array_queue/array_queue_test.go
@@ -13,11 +13,25 @@ func TestNew(t *testing.T) {
 func TestAdd(t *testing.T) {
 	tests := []int{1, 2, 1, -1, -2, -100, 0, 0}
 	aq := New()
-	for i, v := range tests {
-		aq.Add(v)
-		if aq.n != i+1 {
-			t.Errorf("aq.n = %v at %v th Add", aq.n, i+1)
-		}
+	aq.n, aq.cap, aq.j = 8, 8, 0
+	aq.buf = append(aq.buf, tests...)
+	aq.Add(888)
+	if aq.buf[8] != 888 {
+		t.Errorf("ArrayQueue.Add(): %v", aq.buf[8])
+	}
+}
+
+func TestResize(t *testing.T) {
+	tests := []int{1, 2, 1, -1, -2, -100, 0, 0}
+	aq := New()
+	aq.n, aq.cap, aq.j = 8, 8, 3
+	aq.buf = append(aq.buf, tests...)
+	aq.Add(888)
+	if aq.buf[0] != -1 && aq.buf[1] != -2 {
+		t.Error("ArrayQueue.resize() was failed")
+	}
+	if aq.j != 0 {
+		t.Error("ArrayQueue.resize() was failed")
 	}
 }
 
